7f774a21ba5dc32484d9dd7aa1c38f40c63cd2b8
Tristan Espieux <tristan.espieux@gmail.com>
2018-10-19 10:05:06 +0200

    Attack defense and exp

---
+++ Business/Character.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Character
+    {
+        private Random seed = new Random();
+        private int _baseHP;
+        private int _baseCooldown;
+        private int _level = 1;
+        private int _currentExp = 0;
+        private int _baseExp = 16;
+        private int _givenExp = 4;
+        private float expMultiplier = 1.5F;
+        private float hpMultiplier = 1.1F;
+        private float minAttackMultiplier = 1.1F;
+        private float maxAttackMultiplier = 1.1F;
+
+        public string Name { get; set; }
+        public int CurrentHP { get; set; }
+        public int BaseHP
+        {
+            get => _baseHP;
+            set
+            {
+                _baseHP = value;
+                CurrentHP = value;
+            }
+        }
+        public int BaseMinAttack { get; set; }
+        public int CurrentMinAttack
+        {
+            get
+            {
+                int curMinAtk = BaseMinAttack;
+                for (int i = 1; i < _level; ++i)
+                    curMinAtk = (int)(curMinAtk * minAttackMultiplier);
+                return (curMinAtk);
+            }
+        }
+        public int BaseMaxAttack { get; set; }
+        public int CurrentMaxAttack
+        {
+            get
+            {
+                int curMaxAtk = BaseMaxAttack;
+                for (int i = 1; i < _level; ++i)
+                    curMaxAtk = (int)(curMaxAtk * maxAttackMultiplier);
+                return (curMaxAtk);
+            }
+        }
+        public int CurrentCooldown { get; set; }
+        public int BaseCooldown
+        {
+            get => _baseCooldown;
+            set
+            {
+                _baseCooldown = value;
+                CurrentCooldown = value;
+            }
+        }
+        public Character Target { get; set; }
+
+        public int getRequiredExp
+        {
+            get
+            {
+                int givenXP = _baseExp;
+                for (int i = 1; i < _level; ++i)
+                    givenXP = (int)(givenXP * expMultiplier);
+                return (givenXP);
+            }
+        }
+
+        public int getGivenExp
+        {
+            get
+            {
+                return ((int)(getRequiredExp / _givenExp));
+            }
+        }
+
+        private string setExp(int exp)
+        {
+            string result = "";
+
+            int levels = 0;
+            while (exp > 0)
+            {
+                _currentExp += exp;
+                exp = _currentExp - getRequiredExp;
+                if (exp >= 0)
+                {
+                    ++levels;
+                    ++_level;
+                    _currentExp = 0;
+                    BaseHP = (int)(BaseHP * hpMultiplier);
+                }
+            }
+            if (levels > 0)
+                result = "You've earned " + levels + " levels ! You are now level " + _level + "\n";
+            return (result);
+        }
+
+        public int Defend(int damage)
+        {
+            CurrentHP -= damage;
+            if (CurrentHP <= 0)
+                CurrentHP = 0;
+            return (CurrentHP);
+        }
+
+        public string Attack()
+        {
+            string report;
+            CurrentCooldown = _baseCooldown;
+            int damage = seed.Next(CurrentMinAttack, CurrentMaxAttack + 1);
+            int TargetHP = Target.Defend(damage);
+            report = Name + " attacked " + Target.Name + " and dealt " + damage + " damage.\n";
+            report += Target.Name + " has " + TargetHP + " HP remaining.\n";
+            if (TargetHP <= 0)
+            {
+                report += "You killed " + Target.Name + " and have earned " + Target.getGivenExp + " exp\n";
+                report += setExp(Target.getGivenExp);
+            }
+            return (report);
+        }
+
+        public string Stats()
+        {
+            string result = "=== Name : " + Name + " === HP : " + CurrentHP + "/" + BaseHP + " === Damages : " + CurrentMinAttack + " - " + CurrentMaxAttack + " === Level : " + _level + " === Exp : " + _currentExp + "/" + getRequiredExp + " ===\n";
+
+            return (result);
+        }
+    }
+}
--- SomeRPG/Program.cs
+++ SomeRPG/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business;
 
 namespace SomeRPG
 {
@@ -10,8 +11,67 @@ namespace SomeRPG
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo startOVer;
+            do
+            {
+                Character player = new Character
+                {
+                    Name = "Cid",
+                    BaseHP = 42,
+                    BaseCooldown = 10,
+                    BaseMinAttack = 5,
+                    BaseMaxAttack = 10
+                };
 
+                List<Character> monsters = new List<Character>();
+                for (int i = 0; i < 10; ++i)
+                {
+                    monsters.Add(new Character
+                    {
+                        Name = "Noob" + i,
+                        BaseHP = 20,
+                        BaseCooldown = 11,
+                        BaseMinAttack = 2,
+                        BaseMaxAttack = 5
+                    });
+                }
 
+                while (player.CurrentHP > 0 && monsters.Count > 0)
+                {
+                    Console.WriteLine(player.Stats());
+                    var monster = monsters[0];
+                    player.Target = monster;
+                    monster.Target = player;
+                    while (player.CurrentHP > 0 && monster.CurrentHP > 0)
+                    {
+                        --player.CurrentCooldown;
+                        --monster.CurrentCooldown;
+                        if (player.CurrentCooldown <= 0)
+                            Console.WriteLine(player.Attack());
+                        if (monster.CurrentCooldown <= 0)
+                            Console.WriteLine(monster.Attack());
+                    }
+                    if (player.CurrentHP > 0 && monster.CurrentHP <= 0)
+                    {
+                        Console.WriteLine("Well done " + player.Name + ", you raped " + monster.Name + ".\nYou have " + player.CurrentHP + "HP remaining.");
+                        monsters.RemoveAt(0);
+                    }
+                    else if (monster.CurrentHP > 0 && player.CurrentHP <= 0)
+                        Console.WriteLine("You really sux " + player.Name + ", you've been raped by " + monster.Name + ".\nIt has " + monster.CurrentHP + "HP remaining.");
+                    else if (monster.CurrentHP <= 0 && player.CurrentHP <= 0)
+                        Console.WriteLine("HAHA " + player.Name + " and " + monster.Name + " killed each other");
+                    else
+                        Console.WriteLine("If you see this message, both player and monster are alive and the program is buggy");
+                    Console.WriteLine("Press enter");
+                    Console.ReadLine();
+                }
+                do
+                {
+                    Console.WriteLine("Game over. Start over? y/n");
+                    startOVer = Console.ReadKey();
+                } while (startOVer.Key != ConsoleKey.Y
+                         && startOVer.Key != ConsoleKey.N);
+            } while (startOVer.Key == ConsoleKey.Y);
         }
     }
 }
